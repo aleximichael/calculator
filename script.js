18cb7765f3c821921198ee55e1f4bbfed109991b
Alexi Michael <189779678+heyitsalexi@users.noreply.github.com>
2025-03-04 15:32:25 -0500

    Fix: Create overflow container for long nums

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// Key Variables
+let num1 = "";
+let num2 = "";
+let operator = "";
+let numToggle = 1; // switch between 1 (Add to Num1), 2 (Add to Num2),and 0 (Reset Num1) 
+
+// Display Text
+const displayContainer = document.querySelector("#display-container");
+const displayText = document.querySelector("#display-text");
+
+// Keyboard + Button Input
 const calcBtns = document.querySelector('#button-container');
 
 calcBtns.addEventListener('click', (event) => {
     let target = event.target;
+
     switch(target.id) {
         case "num":
             runNumber(target.textContent);
@@ -20,15 +32,16 @@ calcBtns.addEventListener('click', (event) => {
             break;
         case "neg":
             runInverse();
-            break;s
+            break;
     };
 });
 
 document.addEventListener('keydown', (event) => {
-    const numKeys = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0'];
+    const numKeys = ['Digit1', 'Digit2', 'Digit3', 'Digit4',
+        'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0'];
 
     if (event.shiftKey && event.code === 'Digit8') {
-        runOperator('×');
+        runOperator('×'); // Multiplication symbol
     } else if(numKeys.includes(event.code)) {
         runNumber(event.code.slice(-1));
     } else if (event.shiftKey && event.code === 'Equal') {
@@ -36,70 +49,75 @@ document.addEventListener('keydown', (event) => {
     } else if (event.code === 'Equal' || event.code === 'Enter') {
         runEquals();
     } else if (event.code === 'Slash') {
-        runOperator('÷');
+        runOperator('÷'); // Division symbol
     } else if (event.code === 'Minus') {
         runOperator('-');
     } else if (event.code === 'Backspace') {
         runDelete();
+    } else if (event.code === 'Period') {
+        runNumber('.');
     };
 });
 
+// Calculator Functions
+
 function operate() {
+    
+    let result = "";
+
+    // Max total of 10 characters -> Max of 8 decimal places
+    num1 = Math.floor(Number(num1) * 100000000); 
+    num2 = Math.floor(Number(num2) * 100000000);
+
     console.log(num1);
     console.log(num2);
-    console.log(operator);
-    let result = "";
+
     switch (operator) {
         case "+":
-            result = (Number(num1) + Number(num2)) * 10000000 / 10000000;
+            result = (num1 + num2) / 100000000;
             break;
         case "-":
-            result = num1 - num2;
+            result = (num1 - num2) / 100000000;
             break;
         case "÷":
-            if (Number(num2) === 0) {
+            if (num2 === 0) {
                 result = "ERROR";
             } else {
-                result = Math.floor(num1 / num2 * 10000000) / 10000000;
+                result = num1  / num2;
             };
             break;
         case "×":
-            result = num1 * num2;
+            result = num1 * num2 / (100000000 * 100000000);
             break;
     };
-
-    num1 = result.toString();
+    
+    result = result.toString();
+    
+    num1 = result;
     num2 = "";
     operator = "";
     numToggle = 0;
     displayText.textContent = result;
 }
 
-let num1 = "";
-let num2 = "";
-let operator = "";
-let numToggle = 1; // switch between 1 (Add to Num1), 2 (Add to Num2), and 0 (Reset Num1) 
-
-const displayContainer = document.querySelector("#display-container");
-const displayText = document.querySelector("#display-text");
-
 function runNumber(button) {
 
     if (numToggle === 0) {
-        num1 = "";
+        num1 = ""; // reset calculation if a number is entered immediately 
+        // after getting a result
         numToggle = 1;
-    }
+    };
 
     if (numToggle === 1) {
-        if (!(button === "." && num1.includes('.'))) {
+        if (!(button === '.' && num1.includes('.'))) { 
             num1 += button;
             displayText.textContent = num1;
         };
     } else if (numToggle === 2) {
-        if (!(button === "." && num2.includes('.'))) {
+        if (!(button === '.' && num2.includes('.'))) { 
             num2 += button;
             displayText.textContent = num2;
-        };
+        };  
     };
 }
 
@@ -115,7 +133,7 @@ function runOperator (button) {
 
 function runEquals() {
     if (numToggle === 2) {
-        operate();
+        operate(); // only operate if there are two numbers
     };  
 }
 
@@ -128,9 +146,6 @@ function runClear() {
 }
 
 function runDelete() {
-    console.log(num1);
-    console.log(num2);
-
     if (num2 !== "") {
         num2 = num2.slice(0,-1);
     } else if (operator !== "") {
